8898619122a6b655bf3e79a8602fe3a8ad95c88c
NAFI <78622733+Rynardv8@users.noreply.github.com>
2025-09-20 11:27:48 +0200

    Project Settings

--- sajam/Scene's/Player/player.gd
+++ sajam/Scene's/Player/player.gd
@@ -12,8 +12,8 @@ func _ready() -> void:
 		
 
 func _physics_process(delta: float) -> void:
-	var x := Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
-	var y := Input.get_action_strength("ui_down")  - Input.get_action_strength("ui_up")
+	var x := Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
+	var y := Input.get_action_strength("move_down")  - Input.get_action_strength("move_up")
 	var dir := Vector2(x, y).normalized()
 	
 	velocity = dir * move_speed
